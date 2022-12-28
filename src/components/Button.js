5b06216f6d6613ba5fa0e58e27ab82cc6cc3ae87
bhavanarajakram <bhavanarajak78@gmail.com>
2022-12-28 21:39:15 +0530

    added files

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+        <button style={{background:"red"}}>Click Me!</button>
+    </div>
+  )
+}
+
+export default Button
